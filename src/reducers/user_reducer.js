16138224c37785811afc94cbc43a41f712c570bd
Resdan Logan <rezn5447@gmail.com>
2018-03-17 14:25:09 -0700

    update reducer functions to have names

--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -1,6 +1,6 @@
 import { SET_USER } from '../actions/types';
 
-export default function(state = null, action) {
+export default function userReducer(state = null, action) {
 	switch (action.type) {
 		case SET_USER:
 			return action.payload;
