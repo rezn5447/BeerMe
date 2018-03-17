08976bd72f0ff3a2442fd4b8af45a7d08ce1f3d9
Resdan Logan <rezn5447@gmail.com>
2018-03-17 14:25:34 -0700

    uneccessary mobx stuff

--- src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
-
-export default combineReducers({
-	auth: authReducer,
-	user: userReducer
-});
