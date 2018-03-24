d94c3369cbb3f505e4f7ce7723cba8dcc6353ef2
Resdan Logan <rezn5447@gmail.com>
2018-03-24 10:57:15 -0700

    successful render of firebase data via reducer, VALUE

--- src/screens/StadiumScreen.js
+++ src/screens/StadiumScreen.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import {
 	View,
 	Alert,
+	FlatList,
 	ToastAndroid,
 	Text,
 	BackHandler,
 	ImageBackground
 } from 'react-native';
+import { Button } from 'react-native-elements';
+import { firebaseConnect } from 'react-redux-firebase';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { signOut } from '../actions';
-import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import { ContinueButton } from '../components';
 import BackImg from '../assets/bg1.png';
 
@@ -55,24 +57,34 @@ class StadiumScreen extends Component {
 		return true;
 	};
 
+	handleStadiumPress = name => {
+		Alert.alert(`${name} pressed`);
+	};
+
 	renderStadiumList() {
 		const { stadiums } = this.props;
-		if (!isLoaded(stadiums)) {
-			return <Text>No Stadiums rendering</Text>;
-		}
-
-		if (isEmpty(stadiums)) {
-			return <Text>No Stadiums rendering</Text>;
-		}
-
-		return <Text>{stadiums}</Text>;
+		console.log(stadiums);
+		return (
+			<FlatList
+				style={{ margin: 5 }}
+				data={stadiums}
+				renderItem={({ item }) => (
+					<Button
+						key={item.key}
+						title={item.value.label}
+						buttonStyle={styles.stadiumButton}
+						onPress={() => this.handleStadiumPress(item.key.name)}
+					/>
+				)}
+			/>
+		);
 	}
 
 	render() {
 		return (
 			<ImageBackground source={BackImg} style={styles.bgImg}>
 				<View style={styles.container}>
-					<View>
+					<View style={{ height: 300 }}>
 						<Text>Here is where the stadiums go</Text>
 						<Text>{this.props.profile.displayName}</Text>
 						{this.renderStadiumList()}
@@ -96,12 +108,24 @@ const styles = {
 	},
 	bgImg: {
 		flex: 1
+	},
+	stadiumButton: {
+		margin: 2,
+		backgroundColor: '#00bcd4',
+		borderRadius: 5
+	},
+	TextStyle: {
+		fontSize: 25,
+		textAlign: 'center'
 	}
 };
 
 const mapStateToProps = ({ firebase }) => ({
 	profile: firebase.profile,
-	stadiums: null
+	stadiums: firebase.ordered.stadiums
 });
 
-export default connect(mapStateToProps, { signOut })(StadiumScreen);
+export default compose(
+	firebaseConnect(() => [{ path: 'stadiums' }]),
+	connect(mapStateToProps, { signOut })
+)(StadiumScreen);
